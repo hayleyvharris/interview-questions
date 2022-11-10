41605e12d9cf3a14be2677d67fc64a99fad25b8f
Hayley Berkebile <hayley@zirtue.com>
2022-11-10 17:00:59 -0500

    Adding algo questions to scripts dir

---
+++ scripts/arrayFlatten.ts
@@ -0,0 +1,9 @@
+// Given an array that may contain elements that are arrays, ensure it is fully flattened.
+// Assume the nested arrays are n-deep (e.g. we don't know how deep the nesting goes)
+// [1, 2, [3, [4]], 5, [[6, 7], 8]] --> [1, 2, 3, 4, 5, 6, 7, 8]
+
+function flatten(array: []): [] {
+    return [];
+}
+
+export default flatten;
---
+++ scripts/validParentheses.ts
@@ -0,0 +1,11 @@
+// Given a string containing only {,},[,],(,) characters, determine if the sequence is valid.
+// A valid sequence means that for every open brace, bracket and parentheses, there is a corresponding closing one.
+// Note that these open and closing pairs can be nested.
+// ({}[])(){([()])} --> valid
+// ({{)[](){{]} --> invalid
+
+function isValid(sequence: string): boolean {
+    return false;
+}
+
+export default isValid;
