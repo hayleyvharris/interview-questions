5f7035a23c065ff0b796310ce20cb2c137200f11
Hayley Berkebile <hayley@zirtue.com>
2022-11-10 13:06:30 -0500

    setting up users, adding tailwind

---
+++ src/users.tsx
@@ -0,0 +1,27 @@
+import { range } from 'lodash';
+
+enum Roles {
+    USER = 'user',
+    ADMIN = 'admin',
+}
+
+const users = [
+    {
+        name: 'admin',
+        email: 'admin@example.com',
+        username: 'admin',
+        bio: 'Hello I am Admin',
+        role: Roles.ADMIN,
+    },
+    ...range(5).map((i: number) => (
+        {
+            name: `user ${i}`,
+            email: `use${i}1@example.com`,
+            username: `user${i}`,
+            bio: `Hello I am User ${i}`,
+            role: Roles.USER,
+        }
+    )),
+];
+
+export default users;
